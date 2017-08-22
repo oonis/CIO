90615786cb0c0b9780797bbce32bb71fa072e641
Sam Macaluso <sam@macalu.so>
2017-08-22 19:08:40 -0400

    got the camera working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,22 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
-
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 using namespace cv;
-
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-
-    Mat image;
-    image = imread( argv[1], 1 );
-
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-
-    waitKey(0);
-
-    return 0;
+using namespace std;
+ 
+int main() {
+	VideoCapture stream1(0);   //ID 0 for first camera source
+ 
+	if (!stream1.isOpened()) {
+		cout << "Unable to open camera" << endl;
+	}
+ 
+	while (true) {
+		Mat cameraFrame;
+		stream1.read(cameraFrame);
+		imshow("camera", cameraFrame);
+		if (waitKey(30) == 27)
+			break;
+	}
+	return 0;
 }
